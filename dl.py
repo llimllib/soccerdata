4fb0a9ed5d44d56b12a53a059faf3232ad988413
Bill Mill <bill.mill@gmail.com>
2013-11-03 15:02:59 -0500

    start downloading cl data

--- dl.py
+++ dl.py
@@ -1,7 +1,7 @@
-import requests, cPickle, os.path
+import requests, cPickle, os.path, codecs
 from bs4 import BeautifulSoup
 
-def write_csv(outfile, results):
+def write_bpl_csv(outfile, results):
     outfile.write("home, homescore, awayscore, away, date, time\n")
     for result in results:
         outfile.write("{0}, {1}, {2}, {3}, {4}, {5}\n".format(*result))
@@ -26,7 +26,7 @@ def bpl_13_14():
             score = game.find("td", "score").a.text.split("-")
             results.append((home, int(score[0]), int(score[1]), away, date, time))
 
-    write_csv(file("data/bpl_13_14.csv", 'w'), results)
+    write_bpl_csv(file("data/bpl_13_14.csv", 'w'), results)
 
 def bpl_season(season="2012-2013"):
     print "getting season {0}".format(season)
@@ -51,8 +51,61 @@ def bpl_season(season="2012-2013"):
 
     write_csv(file("data/bpl_{0}_{1}.csv".format(season[2:4], season[7:9]), 'w'), results)
 
+def write_cl_csv(outfile, results):
+    outfile.write("home, homescore, awayscore, away, date, group\n")
+    for result in results:
+        outfile.write(u"{0}, {1}, {2}, {3}, {4}, {5}\n".format(*result))
+
+def parse_cl_page(soup):
+    days = soup.find_all("table", "tablehead")
+    results = []
+    for day in days:
+        date = day.tr.text
+        for game in day.find_all("tr")[2:]:
+            cells = game.find_all("td")
+            home = cells[1].text
+            result = cells[2].text
+            away = cells[3].text
+            group = cells[4].text
+
+            homescore, awayscore = result.split("-")
+
+            results.append((home, homescore, awayscore, away, date, group))
+
+    return results
+
+def champions_league():
+    url = "http://espnfc.com/results/_/league/uefa.champions/uefa-champions-league?cc=5901"
+
+    print "getting %s" % url
+    r = requests.get(url)
+    assert r.status_code==200
+
+    soup = BeautifulSoup(r.content)
+    season = soup.h2.text.split(" ")[0]
+    results = parse_cl_page(soup)
+
+    while 1:
+        previous = soup.find("p", "prev-next-links").a
+        if not "Previous" in previous.text: break
+        url = previous["href"]
+
+        print "getting %s" % url
+        r = requests.get(url)
+        assert r.status_code==200
+
+        soup = BeautifulSoup(r.content)
+        if soup.h2.text.split(" ")[0] != season:
+            print "found season: %s" % soup.h2.text.split(" ")[0]
+            break
+        results += parse_cl_page(soup)
+
+    out = codecs.open("data/cl_13_14.csv", 'w', "utf8")
+    write_cl_csv(out, results)
+
 if __name__=="__main__":
     #bpl_13_14()
-    for season in range(2007, 2013):
-        seasonstr = "{0}-{1}".format(season, season+1)
-        bpl_season(seasonstr)
+    #for season in range(1992, 2013):
+    #    seasonstr = "{0}-{1}".format(season, season+1)
+    #    bpl_season(seasonstr)
+    champions_league()
